7e5879c78aa4a916a31d0dc36dc52d16f9801796
Fj <fj.mail@gmail.com>
2019-10-30 19:15:32 +0200

    new functionality

--- programmator/panel.py
+++ programmator/panel.py
@@ -48,31 +48,36 @@ def create_widgets(tabs):
             grid_separator(page)
         return page
 
-    page = add_tab('Параметры', 'Основные параметры')
+    page = add_tab('Настройки', 'Основные настройки')
 
-    ctrl = MMC_BCD(page, 'Номер устройства', 0, 4)
+    ctrl = MMC_BCD(page, 'Номер коммуникатора', 0, 4)
     grid_label_and_control_mmc(ctrl)
 
     MMC_FixedByte(2)
 
-    ctrl = MMC_Choice(page, 'Тип панели', 3, [2, 1, 0], {
+    ctrl = MMC_Choice(page, 'Тип контрольной панели', 3, [2, 1, 0], {
         0: 'Коммуникатор',
         1: 'DALLAS',
-        2: 'ADEMCO',
-        3: 'MAGELAN'})
+        2: 'RING/TIP',
+        3: 'MAGELAN',
+        4: 'SECOLINK',
+        5: 'ESPRIT',
+        })
     grid_label_and_control_mmc(ctrl)
 
 
-    ctrl = MMC_Int(page, 'Период тестовой посылки (сек)', [4, 5])
+    ctrl = MMC_Int(page, 'Период тестовой посылки (мин)', [4, 5])
+    ctrl.mmc.var.set(30) # todo - saner default
     grid_label_and_control_mmc(ctrl)
 
     ctrl = MMC_BCD(page, 'Тестовая посылка', 6, 7) # TODO: 7 can't be right
+    ctrl.mmc.var.set('16A2AAAAA')
     grid_label_and_control_mmc(ctrl)
 
-    ctrl = MMC_Choice(page, 'Рассылка на IP адреса', 3, [5, 4, 3], {
+    ctrl = MMC_Choice(page, 'Раппорт на IP адреса', 3, [5, 4, 3], {
         0b100: 'IP 1',
         0b010: 'IP 2',
-        0b110: 'Основной/резервный',
+        0b110: 'IP1 основной/IP2 резервный',
         0b111: 'IP 1 + IP 2'})
     grid_label_and_control_mmc(ctrl)
 
@@ -118,76 +123,80 @@ def create_widgets(tabs):
         grid_control(ctrl, sticky='w')
 
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'Антидребезг', ctrl, column=2)
+        grid_label_and_control(frame, 'Антидребезг', ctrl, column=2) # 500 ms
 
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'Время срабатывания', ctrl)
+        grid_label_and_control(frame, 'Задержка срабатывания', ctrl) # 0
 
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'Время восстановления', ctrl, column=2)
+        grid_label_and_control(frame, 'Задержка восстановления', ctrl, column=2) # 0
 
         var = tk.IntVar(ctrl)
-        ctrl = tk.Checkbutton(frame, variable=var, text='SMS рассылка')
+        ctrl = tk.Checkbutton(frame, variable=var, text='SMS рассылка') # off
         grid_control(ctrl, sticky='w')
 
+        # Тип зоны - Только под охраной, 24х часовая
         var = tk.IntVar(ctrl)
         ctrl = tk.Checkbutton(frame, variable=var, text='24х часовой')
         grid_control(ctrl, column=2, sticky='w')
 
         var = tk.IntVar(ctrl)
-        ctrl = tk.Checkbutton(frame, variable=var, text='Выход 1 управляется')
+        ctrl = tk.Checkbutton(frame, variable=var, text='управление PGM1') # off
         grid_control(ctrl, sticky='w')
 
         var = tk.IntVar(ctrl)
-        ctrl = tk.Checkbutton(frame, variable=var, text='Выход 2 управляется')
+        ctrl = tk.Checkbutton(frame, variable=var, text='управление PGM2') # off
         grid_control(ctrl, column=2, sticky='w')
 
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'Команда тревоги', ctrl)
+        grid_label_and_control(frame, 'Команда срабатывания', ctrl)
 
         ctrl = tk.Entry(frame)
         grid_label_and_control(frame, 'Команда восстановления', ctrl, column=2)
 
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'Раздел', ctrl)
+        grid_label_and_control(frame, 'Район', ctrl) # 00-99, default 00
 
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'Пользователь', ctrl, column=2)
+        grid_label_and_control(frame, 'Пользователь', ctrl, column=2) # 000-999, 000
 
         grid_separator(frame, False)
 
 
-    page = add_tab('Выходы', 'Конфигурация программируемых выходных сигналов (PGM)')
+    page = add_tab('PGM', 'Конфигурация программируемых выходных сигналов (PGM)')
 
     container = page
 
     for i in range(2):
-        frame = tk.LabelFrame(container, text=f'Выход №{i + 1}')
+        frame = tk.LabelFrame(container, text=f'PGM{i + 1}')
         grid_control(frame, pady=5)
 
         var = tk.IntVar(ctrl)
-        ctrl = tk.Checkbutton(frame, variable=var, text='Выход задействован')
+        ctrl = tk.Checkbutton(frame, variable=var, text='PGM задействован')
         grid_control(ctrl, sticky='w')
 
+        # Режим работы: Потенциальный Импульсный
         var = tk.IntVar(ctrl)
         ctrl = tk.Checkbutton(frame, variable=var, text='Импульсный')
         grid_control(ctrl, sticky='w')
 
+        # Тип выхода: Нормально разомкнут, Нормально замкнут
         var = tk.IntVar(ctrl)
         ctrl = tk.Checkbutton(frame, variable=var, text='Нормально разомкнут')
         grid_control(ctrl, column=2, sticky='w')
 
+        # только в импульсный режим
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'Длительность при постановке', ctrl)
+        grid_label_and_control(frame, 'Длительность при срабатывании', ctrl)
 
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'Длительность при снятии', ctrl, column=2)
+        grid_label_and_control(frame, 'Длительность при восстановлении', ctrl, column=2)
 
         ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'При постановке (импульсный)', ctrl)
+        grid_label_and_control(frame, 'Длительность при постановке/снятии', ctrl)
 
-        ctrl = tk.Entry(frame)
-        grid_label_and_control(frame, 'При снятии (импульсный)', ctrl, column=2)
+        # ctrl = tk.Entry(frame)
+        # grid_label_and_control(frame, 'При снятии (импульсный)', ctrl, column=2)
 
         var = tk.IntVar(ctrl)
         ctrl = tk.Checkbutton(frame, variable=var, text='Программируемый через СМС')
@@ -199,12 +208,13 @@ def create_widgets(tabs):
 
         grid_separator(frame, False)
 
-    page = add_tab('СМС', 'Список телефонов для рассылки СМС')
+    page = add_tab('СМС рассылка', 'Список телефонов пользователей для рассылки СМС')
+    # "обязательно ввести международный код страны" (красным)
     for i in range(10):
         ctrl = tk.Entry(page)
         grid_label_and_control(page, f'Телефон №{i + 1}', ctrl, pady=5)
 
-    page = add_tab('Телефоны', 'Белый список телефонов пользователей и права')
+    page = add_tab('СМС управление', 'Белый список телефонов с правами управления')
     container = tk.Frame(page)
     grid_control(container)
 
@@ -214,7 +224,7 @@ def create_widgets(tabs):
         grid_label_and_control(container, f'Телефон №{i + 1}', ctrl)
 
         ctrl = tk.Entry(container)
-        grid_label_and_control(container, f'PIN', ctrl)
+        grid_label_and_control(container, f'PIN', ctrl) # 0000
 
         frame = tk.LabelFrame(container, text='Права')
         frame.grid(row=row, column=3, rowspan=2, columnspan=2)
